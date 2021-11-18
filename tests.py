8bceb45ccd3d8d18daf2de8d18d5bdcd58b61ab1
dy3l <dy3l@users.noreply.github.com>
2021-11-18 14:16:29 +0100

    Add tests for people, roles, investments and investors

--- tests.py
+++ tests.py
@@ -18,6 +18,35 @@ class Company(BaseModel):
     headquarters: constr(regex=r"Bouskoura|Casablanca|Marrakesh|Rabat|Tangier|TODO")
 
 
+class Person(BaseModel):
+    id: constr(regex=r"[a-z-]+")
+    name: str
+
+
+class Role(BaseModel):
+    person_id: constr(regex=r"[a-z-]+")
+    company_id: constr(regex=r"[a-z-]+")
+    role: constr(regex=r"(co-founder)")
+
+
+class Investor(BaseModel):
+    id: constr(regex=r"[a-z-]+")
+    name: constr(min_length=3, max_length=21)
+    description: constr()
+    website: str
+    founded_date: str
+    headquarters: constr(regex=r"Casablanca|Rabat|TODO")
+
+
+class Investment(BaseModel):
+    company_id: constr(regex=r"[a-z-]+")
+    investor_id: constr(regex=r"[a-z-]+")
+    amount: conint(ge=3000000, le=12000000)
+    currency: constr(regex=r"MAD")
+    type: constr(regex=r"(seed)")
+    raised_date: date
+
+
 def test_companies():
     with open("data/companies.csv", "r", encoding="utf-8") as f:
         reader = csv.DictReader(f, delimiter=",")
@@ -38,5 +67,60 @@ def test_companies():
         )
 
 
-test_companies()
+def test_people():
+    with open("data/people.csv", "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f, delimiter=",")
+        people = list(reader)
+    for person in people:
+        Person(
+            id=person["id"],
+            name=person["name"],
+        )
 
+
+def test_roles():
+    with open("data/roles.csv", "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f, delimiter=",")
+        roles = list(reader)
+    for role in roles:
+        Role(
+            person_id=role["person_id"],
+            company_id=role["company_id"],
+            role=role["role"],
+        )
+
+
+def test_investments():
+    with open("data/investments.csv", "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f, delimiter=",")
+        investments = list(reader)
+    for investment in investments:
+        Investment(
+            company_id=investment["company_id"],
+            investor_id=investment["investor_id"],
+            amount=investment["amount"],
+            currency=investment["currency"],
+            type=investment["type"],
+            raised_date=investment["raised_date"],
+        )
+
+
+def test_investors():
+    with open("data/investors.csv", "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f, delimiter=",")
+        investors = list(reader)
+    for investor in investors:
+        Investor(
+            id=investor["id"],
+            name=investor["name"],
+            description=investor["description"],
+            website=investor["website"],
+            founded_date=investor["founded_date"],
+            headquarters=investor["headquarters"],
+        )
+
+test_companies()
+test_people()
+test_roles()
+test_investments()
+test_investors()
